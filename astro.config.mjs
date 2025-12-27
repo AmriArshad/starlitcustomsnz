7d5d51a3906ac556281a048463535dbe7636fb9e
AmriArshad <amri.arshad16@yahoo.com>
2025-12-27 22:43:06 +1300

    added tailwindcss & ghpages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'starlitcustoms.co.nz', // Your actual domain
+  // If using username.github.io/repo-name, add:
+  // base: '/repo-name',
+});
